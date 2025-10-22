dcecdf0faf4f2c117bc8735dbf98f39afddb028d
Felix Yu <felixyu2627@gmail.com>
2025-10-22 01:12:08 -0700

    Add files via upload

---
+++ histogram.py
@@ -0,0 +1,28 @@
+def histogram(points, bins):
+    n = len(points)
+    k = len(bins)
+
+    binslist = [0] * k
+    pointer = 0
+
+    for i in range(k):
+        a, b = bins[i]
+        while pointer <= n and points[pointer] < b:
+            pointer += 1
+            binslist[i] += 1
+
+        if pointer == n:
+            break
+    
+    rbins = []
+    for i in range(k):
+        width = bins[i][1] - bins[i][0]
+        count = binslist[i]
+
+        if width > 0:
+            density = count / (n * width)
+        else:
+            density = 0
+        rbins.append(density)
+
+    return rbins
