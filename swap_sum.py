e24939b8db3f3d0aa05894b97e3044874a4ae94a
Felix Yu <felixyu2627@gmail.com>
2025-10-22 01:43:01 -0700

    Update swap_sum.py

--- swap_sum.py
+++ swap_sum.py
@@ -1,13 +1,17 @@
 def swap_sum(A, B):
     suma = sum(A)
     sumb = sum(B)
-    need = (suma - sumb + 10) / 2
+    top = suma - sumb + 10
 
+    if top % 2 != 0:
+        return None
+
+    need = top // 2
     i = 0
     j = 0
     a = len(A)
     b = len(B)
-    while i < a & j < b:
+    while i < a and j < b:
         ai = A[i]
         bj = B[j]
         diff = ai - bj
@@ -20,3 +24,5 @@ def swap_sum(A, B):
             j += 1
 
     return None
+
+        
