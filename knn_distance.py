87bdd4d02bde954fb041e9edce235dead6c0a775
Felix Yu <felixyu2627@gmail.com>
2025-10-28 18:04:34 -0700

    Update knn_distance.py

--- knn_distance.py
+++ knn_distance.py
@@ -1,34 +1,23 @@
+import random
+
 def knn_distance(arr, q, k):
-  target_rank = k - 1 
-  low = 0
-  high = n - 1
+    distances = [(abs(x - q), x) for x in arr]
 
-  def partition_by_distance(arr, low, high, q):
-    pivot_index = random.randint(low, high)
-    pivot_distance = abs(arr[pivot_index] - q)
-
-    arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
-    
-    i = low 
-    
-    for j in range(low, high):
-      current_distance = abs(arr[j] - q)
-      if current_distance <= pivot_distance:
-        arr[i], arr[j] = arr[j], arr[i]
-        i += 1
-            
-    arr[i], arr[high] = arr[high], arr[i]
-    return i
-
-  while True:
-    pivot_final_index = partition_by_distance(arr, low, high, q)
-    
-    if pivot_final_index == target_rank:
-      kth_closest_point = arr[pivot_final_index]
-      distance = abs(kth_closest_point - q)
-      return (distance, kth_closest_point)
+    def quickselect(lst, k):
+        if len(lst) == 1:
+            return lst[0]
         
-    elif pivot_final_index < target_rank:
-      low = pivot_final_index + 1
-    else: 
-      high = pivot_final_index - 1
+        pivot = random.choice(lst)[0]
+        lows = [x for x in lst if x[0] < pivot]
+        highs = [x for x in lst if x[0] > pivot]
+        pivots = [x for x in lst if x[0] == pivot]
+
+        if k <= len(lows):
+            return quickselect(lows, k)
+        elif k <= len(lows) + len(pivots):
+            return pivots[0]
+        else:
+            return quickselect(highs, k - len(lows) - len(pivots))
+
+    kth_distance, kth_value = quickselect(distances, k)
+    return kth_distance, kth_value
